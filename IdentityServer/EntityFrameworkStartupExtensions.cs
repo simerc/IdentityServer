65401401097ad71307070427c823bab8409e4c3d
simerc@gmail.com <profiler@vccp.com>
2018-10-31 10:31:48 +0000

    First setup of IdentityServer 4

---
+++ IdentityServer/EntityFrameworkStartupExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public static class EntityFrameworkStartupExtensions
+    {
+        public static void AddEntityFrameworkForIdentityServer(this IServiceCollection services, IConfiguration Configuration)
+        {
+            var connString = Configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                //.AddTestUsers()
+
+                // this adds the config data from DB (clients, resources)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+
+
+                //Add operation data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connString, sql =>
+                            sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                });
+
+        }
+    }
+}
